18b27b2e51eea1fad7be2519e1e069b0ed968ee0
gsilber <silber@udel.edu>
2024-03-13 18:30:20 -0400

    added bootstrap to test code.

--- WebEZ-CLI/component-scaffold/template.component.test.ts
+++ WebEZ-CLI/component-scaffold/template.component.test.ts
@@ -1,7 +1,11 @@
-import { describe, expect, test } from "@jest/globals";
+import { describe, expect, test, beforeAll } from "@jest/globals";
 import { $$$$$$$$Component } from "./########.component";
+import { bootstrap } from "webez";
 
 describe("$$$$$$$$Component", () => {
+    beforeAll(() => {
+        bootstrap<$$$$$$$$Component>($$$$$$$$Component, true);
+    });
     describe("Constructor", () => {
         test("Create Instance", () => {
             const component = new $$$$$$$$Component();
--- WebEZ-CLI/scaffold/src/app/main.component.test.ts
+++ WebEZ-CLI/scaffold/src/app/main.component.test.ts
@@ -1,7 +1,11 @@
-import { describe, expect, test } from '@jest/globals';
+import { describe, expect, test,beforeAll } from '@jest/globals';
 import { MainComponent } from './main.component';
+import {bootstrap} from 'webez';
 
 describe('MainComponent', () => {
+    beforeAll(() => {
+        bootstrap<MainComponent>(MainComponent, true);
+    });    
     describe('Constructor', () => {
         test('Create Instance', () => {
             const component=new MainComponent();
