b7941c554261bf4c67e68febfbdc20983f19e6b4
gsilber <silber@udel.edu>
2024-03-13 18:21:16 -0400

    scaffold new bootstrapper

--- WebEZ-CLI/scaffold/wbcore/start.ts
+++ WebEZ-CLI/scaffold/wbcore/start.ts
@@ -1,11 +1,5 @@
 import '../styles.css';
+import { bootstrap } from '@gsilber/webez';
 
 import { MainComponent } from '../src/app/main.component';
-
-const mainComponent=new MainComponent();
-const target=document.getElementById("main-target")
-if (target)
-	mainComponent.appendToDomElement(target);
-else
-	mainComponent.appendToDomElement(document.body);
-
+bootstrap<MainComponent>(MainComponent);
