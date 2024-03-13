1e4566341f9b3a8e99f0f6975f2a1c2ea58e3fb9
gsilber <silber@udel.edu>
2024-03-13 14:59:55 -0400

    fixed raw-loader

---
+++ WebEZ-CLI/component-scaffold/template.component.test.ts
@@ -0,0 +1,11 @@
+import { describe, expect, test } from "@jest/globals";
+import { $$$$$$$$Component } from "./########.component";
+
+describe("$$$$$$$$Component", () => {
+    describe("Constructor", () => {
+        test("Create Instance", () => {
+            const component = new $$$$$$$$Component();
+            expect(component).toBeInstanceOf($$$$$$$$Component);
+        });
+    });
+});
--- WebEZ-CLI/scaffold/jest/raw-loader.js
+++ WebEZ-CLI/scaffold/jest/raw-loader.js
@@ -2,6 +2,6 @@ const rawloader=require('raw-loader');
 
 module.exports = {
       process(src) {
-        return {code:`module.exports = ${JSON.stringify(rawloader(src))};`};
+        return {code:`module.exports = ${JSON.stringify(rawloader(src))}`}
       }
     }
