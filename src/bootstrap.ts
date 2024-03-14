0c43565e9d6a8bff903d62c3784f1b1b9396bf98
gsilber <silber@udel.edu>
2024-03-13 22:58:46 -0400

    update bootstrap code

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -3,7 +3,7 @@ import { EzComponent } from "./EzComponent";
 declare const window: Window;
 type Constructor<T> = { new (): T };
 const html: string =
-    "<!DOCTYPE html><html><head></head><body><div id='main-target'></div></body></html>";
+    "<div>Testing Environment</div><div id='main-target'></div>";
 
 export function bootstrap<T extends EzComponent>(
     target: Constructor<T>,
