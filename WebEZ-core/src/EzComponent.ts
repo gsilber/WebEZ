b2098d711c0d9c79347bdeaf5da3c9a56e8707c4
gsilber <silber@udel.edu>
2024-03-19 02:31:29 -0400

    adden remaining http methods

--- WebEZ-core/src/EzComponent.ts
+++ WebEZ-core/src/EzComponent.ts
@@ -5,6 +5,10 @@ declare const window: Window;
 export enum HttpMethod {
     GET = "GET",
     POST = "POST",
+    PUT = "PUT",
+    DELETE = "DELETE",
+    PATCH = "PATCH",
+    OPTIONS = "OPTIONS",
 }
 
 /**
