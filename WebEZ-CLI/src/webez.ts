959d7eda6af88ae1ad163c85ecbc9b3cf4688b3c
gsilber <silber@udel.edu>
2024-03-27 13:02:28 -0400

    fixed windows bug in webez-cli

--- WebEZ-CLI/src/webez.ts
+++ WebEZ-CLI/src/webez.ts
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-const version: string = '0.3.10';
+const version: string = '0.3.11';
 
 import fs from "fs";
 import path from "path";
--- WebEZ-CLI/version.sh
+++ WebEZ-CLI/version.sh
@@ -5,3 +5,5 @@ version=$(grep -o '"version": *"[^"]*"' package.json | cut -d'"' -f4)
 
 # Replace strings starting with "const version: string" in ./src/webez.ts
 sed -i "s/const version: string.*/const version: string = '${version}';/" ./src/webez.ts
+sed -i "s/framework for Typescript.*/framework for Typescript (${version})/" ../README.md
+sed -i "s/\"\@gsilber\/webez\"\:.*/\"@gsilber\/webez\"\: \"\^${version}\",/" ./scaffold/package.json
