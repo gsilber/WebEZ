c2ad9738b68983fa42e2432f3a17e6581edc2b11
gsilber <silber@udel.edu>
2024-03-27 17:58:11 -0400

    working tests

--- WebEZ-core/src/bootstrap.ts
+++ WebEZ-core/src/bootstrap.ts
@@ -2,17 +2,18 @@ import { EzComponent } from "./EzComponent";
 
 declare const window: Window;
 
-type Constructor<T> = { new (): T };
+type Constructor<T> = { new (...args: any[]): T };
 
 /** @hidden */
 export function bootstrap<T extends EzComponent>(
     target: Constructor<T>,
     testModeHTML: string = "",
+    ...args: any[]
 ): T {
     if (testModeHTML.length > 0) {
         window.document.body.innerHTML = testModeHTML;
     }
-    let obj: T = Object.assign(new target()) as T;
+    let obj: T = Object.assign(new target(...args)) as T;
     const element = window.document.getElementById("main-target");
 
     if (element) obj.appendToDomElement(element);
